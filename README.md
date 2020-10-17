d7230517c30194f417af93381b0c806c593085f9
Mayank Sharma <mayank.sharma.uk@gmail.com>
2020-10-17 16:31:45 +0100

    Add Readme and rename fetchCompiler

---
+++ README.md
@@ -0,0 +1,2 @@
+## ManimDSL API Backend
+
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 new CompileRequestProcessor().compilerAvailable().then(r => {
     app.listen(port, () => console.log(`Backend service running at http://localhost:${port}`))
 }).catch(() => {
-    console.log("Manim DSL Compiler not downloaded, use fetchLatestCompiler to get the compiler")
+    console.log("Manim DSL Compiler not downloaded, use fetchCompiler to get the compiler")
 })
 
 module.exports = app;
