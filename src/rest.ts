6a433e006ed23c324eaaf2cd0eed2a25b6ecd354
Mayank Sharma <mayank.sharma.uk@gmail.com>
2021-01-03 17:47:58 +0000

    Added request limiter by IP

--- src/rest.ts
+++ src/rest.ts
@@ -2,6 +2,7 @@ import {compileRoutes} from "./routes/compile";
 import {examplesRoutes} from "./routes/examples";
 
 const cors = require('cors');
+const rateLimit = require("express-rate-limit");
 
 export const rest = (): any => {
 
@@ -15,10 +16,16 @@ export const rest = (): any => {
     const app: any = express();
 
     app.use(cors());
-
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(express.static('public'))
+
+    const limiter = rateLimit({
+        windowMs: 60 * 1000,
+        max: 20
+    });
+
+    app.use(limiter)
 
     app.use('/compile', compileRoutes(upload));
     app.use('/examples', examplesRoutes(upload));
